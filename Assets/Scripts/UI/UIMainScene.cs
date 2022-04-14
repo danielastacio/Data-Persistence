2cdab86d890757e7942b4339db0917ddcd19dc36
Daniel Astacio <danielastacio91@gmail.com>
2022-04-14 16:31:06 -0400

    Scene flow and data persistence

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -21,6 +21,10 @@ public class UIMainScene : MonoBehaviour
     protected IUIInfoContent m_CurrentContent;
     protected List<Building.InventoryEntry> m_ContentBuffer = new List<Building.InventoryEntry>();
 
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 
     private void Awake()
     {
